ede7e4238ddd0d404036bb4aab0ec527a2f66a38
vova55555 <vova170545@gmail.com>
2025-11-20 17:15:14 +0200

    12238

---
+++ 14/9837.py
@@ -0,0 +1,9 @@
+from string import printable
+
+for i in printable[:23]:
+    n1 = int(f"7{i}38596", 23)
+    n2 = int(f"14{i}36", 23)
+    n3 = int(f"61{i}7", 23)
+    if (n1+n2+n3)%21 == 0:
+        print((n1+n2+n3)//22)
+        break

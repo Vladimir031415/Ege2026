c5a8526d7b40aa51423e55072f14ef018d34957a
vova55555 <vova170545@gmail.com>
2025-11-20 16:20:32 +0200

    12238

---
+++ 14/1247.py
@@ -0,0 +1,11 @@
+t = "0123456789ABCDEFGHIJK"
+
+
+def Con(a, b):
+    r = ""
+    while a:
+        r += t[a % b]
+        a //= b
+    return r[::-1]
+a= Con(729**8 - 3**13 + 85, 9)
+print(a.count("0"))

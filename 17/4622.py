ce79da5d27cb29fc3219608a51794229462bed7d
Vladimir031415 <vova170545@gmail.com>
2026-03-12 18:51:51 +0200

    4

--- 17/4622.py
+++ 17/4622.py
@@ -1,16 +1,14 @@
-with open(r".\files\17_4597.txt") as file:
+with open(r".\files\17_4622.txt") as file:
     data = [int(i) for i in file]
 
 
 ans = []
 
-m = min(data)
+m = min([x for x in data if x > 0 and x%19 == 0])
 for i in range(len(data) - 1):
     num1, num2 = data[i: i + 2]
-    if num1 % 117 == m or num2 % 117 == m:
+    if num1 + num2 <= m:
         ans.append(num1+num2)
 
 print(len(ans))
 print(max(ans))
-
-

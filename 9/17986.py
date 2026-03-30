b0527d68d74878041170a55e9616b5ef8ee9864e
Vladimir031415 <vova170545@gmail.com>
2026-03-30 16:29:22 +0200

    4

--- 9/17986.py
+++ 9/17986.py
@@ -1,3 +1,2 @@
-with open(r"C:\Users\vova1\PycharmProjects\Ege2026\9\files\17986.txt") as file: data = [list(map(int, i.split())) for i in file]
-print(len([i for i in data if (max(i) < sum(i) - max(i)) and sum([j for j in i if j % 2]) == sum([j for j in i if j % 2 == 0])]))
+print(len([i for i in [list(map(int, i.split())) for i in open(r"C:\Users\vova1\PycharmProjects\Ege2026\9\files\17986.txt")] if (max(i) < sum(i) - max(i)) and sum([j for j in i if j % 2]) == sum([j for j in i if j % 2 == 0])]))
 
